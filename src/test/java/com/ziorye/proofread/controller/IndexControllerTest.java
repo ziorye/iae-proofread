7b8886a875593766894e182aaedc82b7b91b36c0
ziorye <ziorye@gmail.com>
2023-10-30 20:52:58 +0800

    spring.application.name

--- src/test/java/com/ziorye/proofread/controller/IndexControllerTest.java
+++ src/test/java/com/ziorye/proofread/controller/IndexControllerTest.java
@@ -1,14 +1,15 @@
 package com.ziorye.proofread.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,10 +17,18 @@ class IndexControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Autowired
+    Environment env;
+
     @Test
     void index() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString(applicationName)))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString(env.getProperty("spring.application.name"))))
         ;
     }
 }
