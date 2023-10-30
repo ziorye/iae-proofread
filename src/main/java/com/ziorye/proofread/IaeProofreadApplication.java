6d8a981bbfcae7b8b2a549499a24740b3fefaadc
ziorye <ziorye@gmail.com>
2023-10-30 20:35:54 +0800

    New Project by Spring Initializr

---
+++ src/main/java/com/ziorye/proofread/IaeProofreadApplication.java
@@ -0,0 +1,13 @@
+package com.ziorye.proofread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaeProofreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaeProofreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziorye/proofread/IaeProofreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziorye.proofread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaeProofreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
