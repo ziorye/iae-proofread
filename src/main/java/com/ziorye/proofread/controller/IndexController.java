1f3609e2fd151114af8564ba50a67885a677aef2
ziorye <ziorye@gmail.com>
2023-10-30 20:52:46 +0800

    Add spring-boot-starter-thymeleaf
    and update index page

--- src/main/java/com/ziorye/proofread/controller/IndexController.java
+++ src/main/java/com/ziorye/proofread/controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.ziorye.proofread.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/")
     String index() {
-        return "index page";
+        return "index";
     }
 }
--- src/test/java/com/ziorye/proofread/controller/IndexControllerTest.java
+++ src/test/java/com/ziorye/proofread/controller/IndexControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -18,7 +19,7 @@ class IndexControllerTest {
     @Test
     void index() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(content().string("index page"))
+                .andExpect(MockMvcResultMatchers.view().name("index"))
         ;
     }
 }
